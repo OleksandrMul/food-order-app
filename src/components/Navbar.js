cbd3f46ee4315b9712c710310208c6f93b470994
Oleksandr Mul <mulalex834@gmail.com>
2021-12-01 12:38:47 +0100

    finished Up The Nav Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 import logo from '../assets/logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
+import ReorderIcon from '@mui/icons-material/Reorder'
 
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false)
+  function Click() {
+    setShowLinks(!showLinks)
+  }
+
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={showLinks ? 'open' : 'close'}>
         <img src={logo} />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/">Menu</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+        </div>
       </div>
-      <div className="rigthtSide">
+      <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/">Menu</Link>
         <Link to="/">About</Link>
         <Link to="/">Contact</Link>
+        <button onClick={Click}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   )
 }
 
 export default Navbar
+
+
